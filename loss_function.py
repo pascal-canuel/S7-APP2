6352df6fcf270b1f90115e6b0139cf7628f124da
Pascal Canuel <canp2403@usherbrooke.ca>
2023-02-14 16:35:19 -0500

    segmentation is good

--- loss_function.py
+++ loss_function.py
@@ -6,7 +6,8 @@ class LocalizationLoss(nn.Module):
     def __init__(self):
         super(LocalizationLoss, self).__init__()
         self.ce_loss_presence = nn.BCELoss()
-        self.ce_loss_class = nn.CrossEntropyLoss()
+        # self.ce_loss_class = nn.CrossEntropyLoss()
+        self.ce_loss_class = nn.BCELoss(reduction='sum')
         self.mse_loss_xywh = nn.MSELoss()
 
     def forward(self, output, target):
@@ -14,19 +15,35 @@ class LocalizationLoss(nn.Module):
         # target: (N, 3, 5) -> presence, x, y, wh, class index
 
         # Classification
-        output_obj_tresh = output[:, :, 0]
-        target_obj_presence = target[:, :, 0]
+        output_obj_tresh = output[:, :, 0] # Nx3
+        target_obj_presence = target[:, :, 0] # Nx3
         ce_loss_presence_val = self.ce_loss_presence(output_obj_tresh, target_obj_presence)
 
-        output_obj_class = output[:, :, 4:7]
-        target_obj_idx = target[:, :, 4].long()
-        ce_loss_class_val = self.ce_loss_class(output_obj_class, target_obj_idx)
+        # output_obj_class = output[:, :, 4:7]
+        # target_obj_idx = target[:, :, 4].long()
+        # ce_loss_class_val = self.ce_loss_class(output_obj_class, target_obj_idx)
+
+        output_obj_class = output[:, :, 4:7] # Nx3x3
+        
+        target_obj_idx = target[:, :, 4].long() # Nx3
+        target_obj_class = torch.zeros_like(output_obj_class) # Nx3x3
+        for i in range(target_obj_idx.shape[0]): # for each batch
+            for j in range(target_obj_idx.shape[1]): # for each object
+                target_obj_class[i, j, target_obj_idx[i, j]] = 1 and target_obj_presence[i, j]
+
+        ce_loss_class_val = self.ce_loss_class(output_obj_class, target_obj_class)
 
         # Regression
-        output_regression = output[:, :, 1:4]
-        target_regression = target[:, :, 1:4]
+        # output_regression = output[:, :, 1:4]
+        # target_regression = target[:, :, 1:4]
+        #
+        # mse_loss_xywh_val = self.mse_loss_xywh(output_regression, target_regression)
 
-        mse_loss_xywh_val = self.mse_loss_xywh(output_regression, target_regression)
+        Lx = self.mse_loss_xywh(output[:, :, 1], target[:, :, 1])
+        Ly = self.mse_loss_xywh(output[:, :, 2], target[:, :, 2])
+        Lwh = self.mse_loss_xywh(output[:, :, 3].sqrt(), target[:, :, 3].sqrt())
+
+        mse_loss_xywh_val = Lx + Ly + 2*Lwh
 
         # Ponderate
         loss_tot = 5 * mse_loss_xywh_val + ce_loss_presence_val + 0.5 * (1 - ce_loss_presence_val) + ce_loss_class_val
--- models/segmentation_network.py
+++ models/segmentation_network.py
@@ -11,13 +11,13 @@ class SegmentationNetwork(nn.Module):
     def __init__(self):
         super(SegmentationNetwork, self).__init__()
 
-        self.hidden = 32
-
         # Down 1
         self.conv_1_1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=2, padding=0, stride=1)  # 32x52x52
         self.relu_1_1 = nn.ReLU()
         self.conv_1_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=1)  # 32x52x52
         self.relu_1_2 = nn.ReLU()
+        self.conv_1_3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=1)  # 32x52x52
+        self.relu_1_3 = nn.ReLU()
 
         # Down 2
         self.maxpool_2 = nn.MaxPool2d(kernel_size=2, padding=0, stride=2)  # 32x26x26
@@ -25,9 +25,10 @@ class SegmentationNetwork(nn.Module):
         self.relu_2_1 = nn.ReLU()
         self.conv_2_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, stride=1)  # 64x26x26
         self.relu_2_2 = nn.ReLU()
+        self.conv_2_3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, stride=1)  # 64x26x26
+        self.relu_2_3 = nn.ReLU()
 
         # Down 3
-        # TODO: test leaky layer
         self.maxpool_3 = nn.MaxPool2d(kernel_size=2, padding=0, stride=2)  # 64x13x13
         self.conv_3_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=1)  # 128x13x13
         self.relu_3_1 = nn.ReLU()
@@ -43,32 +44,44 @@ class SegmentationNetwork(nn.Module):
         self.relu_3_6 = nn.ReLU()
 
         # Up 4
-        self.upsample_4 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, padding=0, stride=2)  # 64x26x26
+        self.upsample_4 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, padding=0,
+                                             stride=2)  # 64x26x26
+        # Concat
         self.conv_4_1 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1, stride=1)  # 64x26x26
         self.relu_4_1 = nn.ReLU()
         self.conv_4_2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1, stride=1)  # 32x26x26
         self.relu_4_2 = nn.ReLU()
+        self.conv_4_3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=1)  # 32x26x26
+        self.relu_4_3 = nn.ReLU()
 
         # Up 5
-        self.upsample_5 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=2, padding=0, stride=2)  # 32x52x52
+        self.upsample_5 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=2, padding=0,
+                                             stride=2)  # 32x52x52
+        # Concat
         self.conv_5_1 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1, stride=1)  # 32x52x52
         self.relu_5_1 = nn.ReLU()
         self.conv_5_2 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, padding=1, stride=1)  # 16x52x52
         self.relu_5_2 = nn.ReLU()
-        self.conv_5_3 = nn.Conv2d(in_channels=16, out_channels=4, kernel_size=2, padding=1, stride=1)  # 4x53x53
+        self.conv_5_3 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1, stride=1)  # 16x52x52
+        self.relu_5_3 = nn.ReLU()
 
+        self.conv_5_4 = nn.Conv2d(in_channels=16, out_channels=4, kernel_size=2, padding=1, stride=1)  # 4x53x53
     def forward(self, x):
         # Down 1
         y = self.conv_1_1(x)
         y = self.relu_1_1(y)
         y = self.conv_1_2(y)
-        y1 = self.relu_1_2(y)
+        y = self.relu_1_2(y)
+        y = self.conv_1_3(y)
+        y1 = self.relu_1_3(y)
 
         # Down 2
         y = self.maxpool_2(y1)
         y = self.conv_2_1(y)
         y = self.relu_2_1(y)
         y = self.conv_2_2(y)
+        y = self.relu_2_3(y)
+        y = self.conv_2_3(y)
         y2 = self.relu_2_2(y)
 
         # Down 3
@@ -93,6 +106,8 @@ class SegmentationNetwork(nn.Module):
         y = self.relu_4_1(y)
         y = self.conv_4_2(y)
         y = self.relu_4_2(y)
+        y = self.conv_4_3(y)
+        y = self.relu_4_3(y)
 
         # Up 5
         y = self.upsample_5(y)
@@ -102,8 +117,9 @@ class SegmentationNetwork(nn.Module):
         y = self.conv_5_2(y)
         y = self.relu_5_2(y)
         y = self.conv_5_3(y)
+        y = self.relu_5_3(y)
+        y = self.conv_5_4(y)  # 32x4x53x53
 
-        # out 32x4x53x53
-        out = torch.squeeze(y)
+        out = y
 
         return out
